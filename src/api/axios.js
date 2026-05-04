e30e6c970eae97d64181c5fd6537fbaa25dd74f1
lina <kitounilina180@gmail.com~>
2026-05-04 21:21:59 +0100

    connect frontend to production backendر

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', 
+  // تم تغيير الرابط من localhost إلى رابط Render الجديد
+  baseURL: 'https://stag-backend-z7vh.onrender.com', 
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    
     config.headers.Authorization = `Bearer ${token}`; 
   }
   return config;
